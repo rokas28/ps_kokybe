9c2af17cc106fdddad7ebf2ad8d54e06a3566d41
RokasT <rokas.tamulevicius28@gmail.com>
2021-11-03 17:52:55 +0200

    fixed CI issues

--- User.java
+++ User.java
@@ -1,57 +1,52 @@
 import java.util.ArrayList;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
+ * This is User class that has three public methods for getting user
+ * first name, last name or child name 
  *
  *
  * @author 56130
  */
 public class User {
 
-    private  int id ;
-    private String firstName;
-     private String lastName;
-     private int age;
-    private String profession;
+    private final int id ;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final String profession;
     private ArrayList<User> children;
 
-    public User(int id, String firstName, String lastName, int age) {
-
+    public User(int id, String firstName, String lastName, int age, String profession) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.profession = profession;
-
     }
 
     public String getChildName(User child) {
-        if (!this.children.contains(child)) {
-            new Exception("Invalid argument!");
-        } else {
-            String name = null;
-            if (child.getFirstName() != null) {
-                name = child.getFirstName();
+        if (this.children.contains(child)) {
+					  String name = null;
+            if (child.getUserFirstName() != null) {
+                name = child.getUserFirstName();
             }
             if (name == "Harry") {
-                name.replace('r', 'p');
+                name = name.replace('r', 'p');
             }
-            if (name != null || name.length() > 0) {
-                name.concat(child.getLastName());
+            if (name != null && name.length() > 0) {
+                name = name.concat(child.getUserLastName());
             }
+        } else {
+        		new Exception("Invalid argument!");
         }
-        return this.getChildName(child);
+        return name;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getUserFirstName() {
+        return this.firstName;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getUserLastName() {
+        return this.lastName;
     }
 }
